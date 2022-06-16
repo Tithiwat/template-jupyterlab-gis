47a7e3dd8c8053701134fe4db8af8a53aa2b5794
Tithiwat <tithiwattong@gmail.com>
2022-06-16 11:12:20 +0700

    add deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 VER=$(cat version.py)
 docker-compose build
 docker tag tithiwat/template-jupyterlab-gis tithiwat/template-jupyterlab-gis:$VERSION
-docker push tithiwat/template-jupyterlab-gis:latest
 docker push tithiwat/template-jupyterlab-gis:$VERSION
+docker tag tithiwat/template-jupyterlab-gis:$VERSION tithiwat/template-jupyterlab-gis:latest
+docker push tithiwat/template-jupyterlab-gis:latest
