b8b218d581a870329bbbae6ac779764ae86f0597
tithiwat <tithiwattong@gmail.com>
2022-09-15 13:25:49 +0700

    add -y option in init script

--- init-ubuntu-20.04.sh
+++ init-ubuntu-20.04.sh
@@ -4,7 +4,7 @@ sudo apt install -y apt-transport-https ca-certificates curl software-properties
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt install -y docker-ce
 # sudo systemctl status docker
 sudo usermod -aG docker ${USER}
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
